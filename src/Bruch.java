ebfacf132b3de50ba1e37bb2f08646e92452391d
MatthiasSchober <52110387@edu.campus02.at>
2021-11-07 20:45:56 +0100

    Test erfolgreich durchgeführt

--- src/Bruch.java
+++ src/Bruch.java
@@ -29,12 +29,13 @@ public class Bruch {
 
     public Bruch multiplicate(Bruch b2) {
         /* TODO:
-        *  Erstellen Sie für das Ergebnis eine neue Bruch-Variable
-        *   => Übergeben Sie im Konstruktur für numerator den
-        *       eigenen numerator multipliziert mit b2.getNumerator()
-        *   => Selbige auch für denominator
-        *  Retournieren Sie ihre neue Bruch-Variable
-        * */
+         *  Erstellen Sie für das Ergebnis eine neue Bruch-Variable
+         *   => Übergeben Sie im Konstruktur für numerator den
+         *       eigenen numerator multipliziert mit b2.getNumerator()
+         *   => Selbige auch für denominator
+         *  Retournieren Sie ihre neue Bruch-Variable
+         * */
+
         Bruch b = new Bruch(this.numerator * b2.getNumerator(),
                 this.denominator * b2.getDenominator());
 
